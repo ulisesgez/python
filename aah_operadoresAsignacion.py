d2511e5689cf17ac4e60c193398b50989e8db9a5
ulisesGutierrez <techwithulises@gmail.com>
2023-03-22 17:30:38 -0600

    comparacion

---
+++ aah_operadoresAsignacion.py
@@ -0,0 +1,19 @@
+#asignamos:
+variableA = 10
+print(variableA)
+
+#reasignacion, ahora sera el valor de 11:
+variableA = variableA + 1
+print(variableA)
+
+#sintaxis reducida al reasignar, equivalente a lo anterior (Nos dara 12):
+variableA += 1
+print(variableA)
+
+#nos dara 10
+variableA -= 2
+print(variableA)
+
+#nos dara 30:
+variableA *= 3
+print(variableA)
---
+++ aai_operadoresComparacion.py
@@ -0,0 +1,28 @@
+a = 4
+b = 2
+c = '2'
+d = 2
+
+result = a == b
+#false
+print(result)
+
+result = b == c
+#false
+print(result)
+
+result = a != b
+#true
+print(result)
+
+result = b != d
+#false
+print(result)
+
+result = a > b
+#true
+print(result)
+
+result = b >= d
+#true
+print(result)
