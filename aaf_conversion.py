a43e5b5612059aa41521cf7bb10c7ff5620173af
ulisesGutierrez <techwithulises@gmail.com>
2023-03-23 09:55:22 -0600

    conversion

--- aaf_conversion.py
+++ aaf_conversion.py
@@ -12,3 +12,23 @@ numeroTres = int(input('Introduce primer numero: '))
 numeroCuatro = int(input('Introduce segundo numero: '))
 result = numeroTres + numeroCuatro
 print('El resultado de la suma', result)
+
+#numero a texto:
+print('-----> conversion <-----')
+numero = int(input('Proporciona un valor del 1 al 3: '))
+numeroTexto = ''
+if numero == 1:
+    numeroTexto = 'numero uno'
+elif numero == 2:
+    numeroTexto = 'numero dos'
+elif numero == 3:
+    numeroTexto = 'numero tres'
+else:
+    numeroTexto = 'valor fuera de rango'
+
+print('-----> conversion <-----')
+numeroUno = 1
+numeroDos = 2
+print('numero 1: ', numeroUno, 'numero 2: ', numeroDos)
+print('suma: ', numeroUno + numeroDos)
+print('concatenacion: ', str(numeroUno) + str(numeroDos))
