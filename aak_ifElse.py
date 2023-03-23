92ef090f38fa593ed29c963ee4b0043d02854c01
ulisesGutierrez <techwithulises@gmail.com>
2023-03-23 09:31:03 -0600

    ifElse

---
+++ aak_ifElse.py
@@ -0,0 +1,17 @@
+#verdadero
+condicion = True
+
+if condicion:
+    print('verdadero')
+else:
+    print('falsa')
+
+"""
+otro ejemplo con elif
+if condicion == True:
+    print("verdadero")
+elif condicion == False:
+    print('falsa')
+else:
+    print('condicion no reconocida')
+"""
