56f5b22a869146651bb768d2aab0666ecb0ce883
ulisesGutierrez <techwithulises@gmail.com>
2023-03-28 09:46:05 -0600

    estructuraControl

--- aak_ifElse.py
+++ aak_ifElse.py
@@ -1,3 +1,39 @@
+#Estructuras de control:
+#En este ejemlo prepaaremos una bebida:
+print('-----> Preparar bebida: <-----')
+print('voy a la cocina')
+print('abrir nevera')
+
+hayLeche = input('Hay leche? s/n ')
+hayChocolate = input('Hay Chocolate? s/n ')
+
+if hayLeche == 's':
+    print('sacamos la leche de la nevera')
+else:
+    print('Ha ir por leche')
+    irPorLeche = input('Enserio ire por leche? ')
+    if irPorLeche == 's':
+        print('He ido por leche')
+    else:
+        print('Preparare otra cosa, y anotare faltante de leche para comprar')
+
+if hayChocolate == 's':
+    print('ponemos leche y chocolate en el vaso')
+    print('Mezclamos')
+    print('Tomamos')
+else:
+    print('Ha ir por chocolate')
+    irPorChocolate = input('Enserio ire por chocolate? ')
+    if irPorChocolate == 's':
+        print('He ido por chocolate')
+        print('ponemos leche y chocolate en el vaso')
+        print('Mezclamos')
+        print('Tomamos')
+    else:
+        print('Preparare otra cosa, y anotare faltante de chocolate para comprar')
+
+print('-----> Fin Preparar bebida: <-----')
+
 #verdadero
 condicion = True
 
