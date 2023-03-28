e0f7da454e7c170c9b95bbea0780d4f13eb16325
ulisesGutierrez <techwithulises@gmail.com>
2023-03-28 12:48:21 -0600

    ciclos

--- aal_ciclos.py
+++ aal_ciclos.py
@@ -1,4 +1,4 @@
-#while:
+print('-----> while <-----')
 """
 #Se imprime infinitamente 'ejecutando while', ya que siempre es true:
 condicion = True
@@ -17,7 +17,23 @@ while contador < 3:
 else:
     print('fin ciclo while')
 
-#for:
+print('-----> while <-----')
+#Inicializar variable a nada.
+respuesta = None
+
+while respuesta != 'a' and respuesta != 'b' and respuesta != 'c':
+    respuesta = input('Que opcion prefieres a), b) y c): ')
+
+if respuesta == 'a':
+    print('has elegido bien')
+elif respuesta == 'b':
+    print('pudiste haber elegido mejor')
+elif respuesta == 'c':
+    print('elegiste mal')
+else:
+    print('no me has dado una rspuesta con sentido')
+
+print('-----> for <-----')
 cadena = 'hola'
 
 for letra in cadena:
