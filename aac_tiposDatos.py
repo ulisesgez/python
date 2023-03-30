b64cdd0365b829263635ccf5ef9cd6a44212cebe
ulisesGutierrez <techwithulises@gmail.com>
2023-03-30 09:57:55 -0600

    precisionDecimal

--- aac_tiposDatos.py
+++ aac_tiposDatos.py
@@ -16,6 +16,21 @@ print(type(numero))
 print('-----> flotantes <-----')
 flotante = 5.5
 print(type(flotante))
+#precision decimal:
+a = 3.3
+print(a)
+b = 1.1 + 2.2
+print(b)
+print(a == b)#False
+#entonces como los comparamos, de forma brusca podemos recortar la presicion, pero este pasa a ser un string:
+bStr = format(b, ".2g")
+print(bStr)
+#no podemos comparar un string con entero, asi que:
+print(bStr == str(a))#True
+
+#ahora de una forma mas amtematica:
+tolerance = 0.00001
+print(abs(a - b) < tolerance)#True
 
 #boolean:
 print('-----> boolean <-----')
