058f7bb2b2e190017531baa7e12404205ea9766a
ulisesGutierrez <techwithulises@gmail.com>
2023-03-29 21:10:32 -0600

    parametros

--- aar_funcion.py
+++ aar_funcion.py
@@ -6,3 +6,11 @@ def miFuncion():
 
 #mandar a llamar:
 miFuncion()
+
+#parametros:
+def miFuncion(nombre, apellido):
+    print(f'{nombre} {apellido}')
+
+#mandar a llamar:
+miFuncion('ulises', 'gutierrez')
+miFuncion('juan', 'perez')
