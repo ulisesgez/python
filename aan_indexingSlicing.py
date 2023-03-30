5b9cd6db5a18bb0e50ef360fe653f08bf30c0ebd
ulisesGutierrez <techwithulises@gmail.com>
2023-03-30 15:43:53 -0600

    correccion

---
+++ aan_indexingSlicing.py
@@ -0,0 +1,15 @@
+print('-----> indexing <-----')
+languaje = 'phyton'
+print(languaje[0])
+print(languaje[1])
+print(languaje[2])
+print(languaje[3])
+print(languaje[4])
+print(languaje[5])
+
+print('-----> slicing <-----')
+print(languaje[0:6])#no tomamos la ultima posicion 6, es antes en la 5
+print(languaje[0:])#va al final
+print(languaje[0:])#inicio al final
+print(languaje[0:6:2])#saltos de 2 en este caso
+print(languaje[::2])#saltos de 2 en este caso
