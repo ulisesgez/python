7b2520b921314ace273762867c576bf966cd76d1
ulisesGutierrez <techwithulises@gmail.com>
2023-03-30 09:33:52 -0600

    args

--- aar_funcion.py
+++ aar_funcion.py
@@ -27,3 +27,32 @@ def restar(c = 10, d = 2):
 resRes = restar()
 print(resRes)
 print(restar(15, 5))
+
+#argumentos variables:
+#no sabemos el numero de parametros, anteponemos el asterisco:
+def listarNombres(*nombres):
+    for nombre in nombres:
+        print(nombre)
+
+listarNombres('juan', 'mises', 'ulises', 'jesus')
+listarNombres('maria', 'jose')
+
+#otro ejemplo, ahora con args, es lo mismo solo es un distintivo, podriamos poner *miTupla por ejemplo:
+def sumarValores (*args):
+    resultado = 0
+    #iteramos:
+    for valor in args:
+        #resultado = resultado + valor:
+        resultado += valor
+    return resultado
+
+print(sumarValores(5, 5, 5))
+
+#otro ejemplo *args
+def multiplicarValores(*numeros):
+    resultado = 1
+    for numero in numeros:
+        resultado *= numero
+    return resultado
+
+print(multiplicarValores(2, 2, 2, 2, 2, 2, 2))
