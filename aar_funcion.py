5f7b3db9a8a8c2eea93d09a71ac474e68a90b1db
ulisesGutierrez <techwithulises@gmail.com>
2023-03-29 22:03:01 -0600

    valoresDefault

--- aar_funcion.py
+++ aar_funcion.py
@@ -16,7 +16,14 @@ miFuncion('ulises', 'gutierrez')
 miFuncion('juan', 'perez')
 
 #return:
-def suma(a, b):
+def sumar(a, b):
     return a + b
-resultado = suma(5, 3)
-print(resultado)
+resSum = sumar(5, 3)
+print(resSum)
+
+#valores por default:
+def restar(c = 10, d = 2):
+    return c - d
+resRes = restar()
+print(resRes)
+print(restar(15, 5))
