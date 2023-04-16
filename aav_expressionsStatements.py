ea6865da49953213fecc5c3538eff7709c5e3766
ulisesGutierrez <techwithulises@gmail.com>
2023-04-15 20:22:27 -0600

    espresiones

---
+++ aav_expressionsStatements.py
@@ -0,0 +1,9 @@
+x = ['Hello', 'Python']
+list('<{}>'.format(x[1][:2].upper() * 2))
+print(x)
+print(x[1])
+print(x[1][:2])
+print(x[1][:2].upper())
+print(x[1][:2].upper() * 2)
+'<{}>'.format(x[1][:2].upper() * 2)
+list('<{}>'.format(x[1][:2].upper() * 2))
