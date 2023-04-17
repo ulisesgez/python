b6fd31e312d5e9656ad8defc611700d3fe0e9747
ulisesGutierrez <techwithulises@gmail.com>
2023-04-17 17:01:54 -0600

    range

--- aao_breakContinue.py
+++ aao_breakContinue.py
@@ -8,7 +8,16 @@ else:
     print('fin ciclo')
 
 print('-----> sin continue <-----')
-#range, funcion de rango, en este caso va del 0 a 9
+
+#range, funcion de rango:
+print(range(3))
+print(range(1, 10, 2))
+print(list(range(3)))
+print(list(range(5, 10)))
+print(list(range(1, 10, 2)))
+
+
+#en este caso la funcion de rango va del 0 a 9
 for i in range(10):
     if i % 2 == 0:
         print(i)
