0a8c2517a724d4ab805c010ec21c445f6f7346d8
ulisesGutierrez <techwithulises@gmail.com>
2023-04-30 16:55:47 -0600

    funcion

--- funcion.py
+++ funcion.py
@@ -84,7 +84,116 @@ Cualquiera. Pronto te mostraremos que print() puede operar con prácticamente to
 3. ¿Qué valor devuelve la función print()?
 
 Ninguno. Su efecto es suficiente.
+
+Usando múltiples argumentos
+Hasta ahora hemos probado el comportamiento de la función print() sin argumentos y con un argumento. También vale la pena intentar alimentar a la función print() con más de un argumento.
+
+Mira la ventana del editor. Esto es lo que vamos a probar ahora:
+
 """
+print("La Witsi Witsi Araña" , "subió" , "a su telaraña.")
+"""
+Hay una invocación de la función print(), pero contiene tres argumentos. Todos ellos son cadenas.
+
+Los argumentos están separados por comas. Los hemos rodeado de espacios para hacerlos más visibles, pero no es realmente necesario, y no lo haremos más.
+
+En este caso, las comas al separar los argumentos juega un papel completamente diferente al de la coma dentro de la cadena. El primero es parte de la sintaxis de Python, mientras que el segundo está diseñado para mostrarse en la consola.
+
+Si observas el código nuevamente, verás que hay no hay espacios dentro de las cadenas.
+
+Los espacios, eliminados de las cadenas, han vuelto a aparecer. ¿Puedes explicar por qué?
+
+Dos conclusiones emergen de este ejemplo:
+
+La función print() invocada con más de un argumento los muestra todos en una sola línea.
+La función print() pone un espacio entre los argumentos de salida por iniciativa propia.
+
+Argumentos posicionales
+Ahora que sabes un poco sobre las costumbres de la función print(), te mostraremos cómo cambiarlas.
+
+Deberías poder predecir la salida sin ejecutar el código en el editor.
+"""
+print("Mi nombre es", "Python.")
+print("Monty Python.")
+
+"""
+La forma en que estamos pasando los argumentos a la función print() es la más común en Python, y se llama la forma posicional. Este nombre proviene del hecho de que el significado del argumento está dictado por su posición (por ejemplo, el segundo argumento se mostrará después del primero, no al revés).
+
+Ejecuta el código y comprueba si el resultado coincide con tus predicciones.
+
+Argumentos de palabra clave
+Python ofrece otro mecanismo para el paso de argumentos, que puede ser útil cuando deseas convencer a la función print() para que cambie un poco su comportamiento.
+
+No vamos a explicarlo en profundidad ahora. Planeamos hacerlo cuando hablemos de funciones. Por ahora, simplemente queremos mostrarte cómo funciona. Siéntete libre de usarlo en tus propios programas.
+
+El mecanismo se llama argumentos de palabras clave. El nombre proviene del hecho de que el significado de estos argumentos se toma no de su ubicación (posición) sino de la palabra especial (palabra clave) utilizada para identificarlos.
+
+La función print() tiene dos argumentos de palabra clave que puedes usar para tus propósitos. El primero se llama end.
+
+En la ventana del editor puedes ver un ejemplo muy simple de cómo usar un argumento de palabra clave.
+"""
+print("Mi nombre es", "Python.", end=" ")
+print("Monty Python.")
+
+"""
+Para usarlo, es necesario conocer algunas reglas:
+
+Un argumento de palabra clave consta de tres elementos: una palabra clave se identifica el argumento (end aquí); un signo de igual (=); y un valor asignado a ese argumento;
+cualquier argumento de palabra clave debe colocarse después del último argumento posicional (esto es muy importante)
+En nuestro ejemplo, hemos utilizado el argumento de palabra clave end, y lo hemos configurado como cadena que contiene un espacio.
+
+Como puedes ver, el argumento de palabra clave end determina los caracteres que la función print() envía a la salida una vez que llega al final de sus argumentos posicionales.
+
+El comportamiento predeterminado refleja la situación en la que el argumento de palabra clave end se usa implícitamente de la siguiente manera: end="\n".
+
+
+Y ahora es el momento de intentar algo más difícil.
+
+Si miras con atención, verás que hemos usado el argumento end, pero la cadena que se le asignó está vacía (no contiene ningún carácter).
+
+¿Qué sucederá ahora? Ejecuta el programa en el editor para averiguarlo.
+"""
+print("Mi nombre es ", end="")
+print("Monty Python.")
+
+"""
+Como el argumento end se ha establecido a nada, la función print() tampoco genera nada, una vez que se han agotado sus argumentos posicionales.
+
+Nota: no se han enviado líneas nuevas a la salida.
+
+La cadena asignada al argumento de palabra clave end puede ser de cualquier longitud. Experimenta con él si quieres.
+
+Dijimos anteriormente que la función print() separa sus argumentos de salida con espacios. Este comportamiento también se puede cambiar.
+
+El argumento de palabra clave que puede hacer esto se denomina sep (como en separador).
+
+Mira el código en el editor, y ejecútalo.
+"""
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+
+"""
+La función print() ahora usa un guión, en lugar de un espacio, para separar los argumentos de salida.
+
+Nota: el valor del argumento sep también puede ser una cadena vacía. Pruébalo tu mismo.
+
+Ambos argumentos de palabra clave pueden mezclarse en una invocación, como aquí en la ventana del editor.
+
+
+"""
+print("Mi", "nombre", "es", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+"""
+
+El ejemplo no tiene mucho sentido, pero presenta de forma visible las interacciones entre end y sep.
+
+¿Puedes predecir el resultado?
+
+Ejecuta el código y comprueba si coincide con tus predicciones.
+
+Ahora que comprendes la función print(), estás listo para considerar cómo almacenar y procesar datos en Python.
+
+Sin print(), no podrías ver ningún resultado."""
 
 print('-----> def <-----')
 #definor funcion:
