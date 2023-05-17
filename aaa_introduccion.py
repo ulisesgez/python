d5f96ca50a6895088493f94dd3b12bc3f707e145
ulisesGutierrez <techwithulises@gmail.com>
2023-05-16 20:47:23 -0600

    comentarios

--- aaa_introduccion.py
+++ aaa_introduccion.py
@@ -590,4 +590,37 @@ print("Vino la lluvia y se la llevó.")
 Como puedes ver, la invocación vacía de print() no está tan vacía como podrías haber esperado - genera una línea vacía o (esta interpretación también es correcta) genera una nuevalínea.
 
 Esta no es la única forma de producir una nuevalínea en la consola de salida. Ahora le mostraremos otra manera.
+
+Comentarios – ¿por qué, cuándo, y dónde?
+
+Quizá en algún momento será necesario poner algunas palabras en el código dirigidas no a Python, sino a las personas quienes estén leyendo el código con el fin de explicarles como es que funciona, o tal vez especificar el significado de las variables, también para documentar quien es el autor del programa y en que fecha fue escrito.
+
+Un texto insertado en el programa el cual es, omitido en la ejecución, es denominado un comentario.
+
+¿Cómo se colocan este tipo de comentarios en el código fuente? Tiene que ser hecho de cierta manera para que Python no intente interpretarlo como parte del código.
+
+Cuando Python se encuentra con un comentario en el programa, el comentario es completamente transparente - desde el punto de vista de Python, el comentario es solo un espacio vacío, sin importar que tan largo sea.
+
+En Python, un comentario es un texto que comienza con el símbolo # y se extiende hasta el final de la línea.
+
+Si se desea colocar un comentario que abarca varias líneas, se debe colocar este símbolo en cada línea. Justo como en el siguiente código:
+"""
+# Este programa evalúa la hipotenusa c. # a y b son las longitudes de los cátetos. a = 3.0 b = 4.0 c = (a ** 2 + b ** 2) ** 0.5 # Se emplea ** en lugar de una raíz cuadrada. print("c =", c)
+"""
+Los desarrolladores buenos y responsables describen cada pieza importante de código, por ejemplo, el explicar el rol de una variables. Aunque la mejor manera de comentar una variable es dándole un nombre que no sea ambiguo.
+
+Por ejemplo, si una variable determinada esta diseñada para almacenar el área de un cuadrado, el nombre square_area será muchísimo mejor que aunt_jane.
+
+El nombre dado a la variable se puede definir como auto-comentable.
+
+Marcar fragmentos de código
+Los comentarios pueden ser útiles en otro aspecto - se pueden utilizar para marcar un fragmento de código que actualmente no se necesita, cual sea la razón. Observa el siguiente ejemplo, sí se descomenta la línea resaltada, esto afectara la salida o resultado del código:
+
+"""
+# Este es un programa de prueba. x = 1 y = 2 # y = y + x print(x + y)
+"""
+Esto es frecuentemente realizado cuando se esta probando un programa, con el fin de aislar un fragmento de código donde posiblemente se encuentra un error.
+
+  Consejo  
+Si deseas comentar o descomentar rápidamente varias línea(s) de código, selecciona las líneas que deseas modificar y utiliza el siguiente método abreviado de teclado: CTRL + / (Windows) or CMD + / (Mac OS). Es un truco muy útil, ¿no? Ahora experimenta con el código en el editor.
 """
