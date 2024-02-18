516a4252f3d383017527c0030aabdcc478d179ea
ulisesGez <gu362937@uaeh.edu.mx>
2024-02-18 17:19:05 -0600

    rename

--- home/aab_helloWorld.py
+++ home/aab_helloWorld.py
@@ -2,7 +2,8 @@
 Una cadena como el argumento de la función print()
 El único argumento entregado a la función print() en este ejemplo es una cadena:
 """
-print("¡Hola, Mundo!")
+print("¡Hello World!")
+print("Mi primer programa en Python")
 """
 Como puedes ver, la cadena está delimitada por comillas - de hecho, las comillas
 forman la cadena, recortan una parte del código y le asignan un significado diferente.
@@ -20,10 +21,3 @@ pero por ahora, esta será suficiente.
 El nombre de la función (print en este caso) junto con los paréntesis y los argumento(s),
 forman la invocación de la función.
 """
-#
-#
-#
-#
-#
-#
-#
