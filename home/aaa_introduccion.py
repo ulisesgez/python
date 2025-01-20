433cb147c394b6cc3fe19caf26be6fe447fd2591
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-01-19 23:10:25 -0600

    space

--- home/aaa_introduccion.py
+++ home/aaa_introduccion.py
@@ -261,7 +261,6 @@ fuente los programas codificados con ellos se denominan scripts. Bien, conozcamo
 a Python.
 
 ¿Qué es Python?
-
 Python es un lenguaje de programación de alto nivel, interpretado, orientado a
 objetos y de uso generalizado con semántica dinámica, que se utiliza para la
 programación de propósito general.
